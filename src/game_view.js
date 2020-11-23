4712d98eaceb5c758d5b4d6447425b55ff043f50
nicolanzi1 <nicolanzi@gmail.com>
2020-11-23 17:38:10 +0000

    Updates

---
+++ src/game_view.js
@@ -0,0 +1,40 @@
+function GameView(game, ctx) {
+    this.ctx = ctx;
+    this.game = game;
+    this.ship = this.game.addShip();
+}
+
+GameView.MOVES = {
+    w: [0, -1],
+    a: [-1, 0],
+    s: [0, 1],
+    d: [1, 0],
+};
+
+GameView.prototype.bindKeyHandlers = function bindKeyHandlers() {
+    const ship = this.ship;
+
+    Object.keys(GameView.MOVES).forEach(function(k) {
+        const move = GameView.MOVES[k];
+        key(k, function () { ship.power(move); });
+    });
+
+    key("space", function () { ship.fireBullet(); });
+};
+
+GameView.prototype.start = function start() {
+    this.bindKeyHandlers();
+    this.lastTime = 0;
+    requestAnimationFrame(this.animate.bind(this));
+};
+
+GameView.prototype.animate = function animate(time) {
+    const timeDelta = time - this.lastTime;
+
+    this.game.step(timeDelta);
+    this.game.draw(this.ctx);
+    this.lastTime = time;
+    requestAnimationFrame(this.animate.bind(this));
+}
+
+module.exports = GameView;
